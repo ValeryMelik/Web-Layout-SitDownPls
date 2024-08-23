d1fa6cfdcacd89bd086d1bfe15cc4feece33c02c
Valera <negrusti98@gmail.com>
2024-08-24 00:15:25 +0300

    for pages

--- src/js/card.js
+++
@@ -1,9 +0,0 @@
-import './modules/header/accordeon-main.js';
-import './modules/header/burger.js';
-import './modules/header/choices.js';
-import './modules/header/find.js';
-import './modules/header/smooth-scroll.js';
-
-import './modules/main/modal.js';
-import './modules/swiper/swiper-card.js';
-import './modules/validator/validator-card.js';
--- src/js/cooperation.js
+++
@@ -1,5 +0,0 @@
-import './modules/header/accordeon-main.js';
-import './modules/header/burger.js';
-import './modules/header/choices.js';
-import './modules/header/find.js';
-import './modules/header/smooth-scroll.js';
